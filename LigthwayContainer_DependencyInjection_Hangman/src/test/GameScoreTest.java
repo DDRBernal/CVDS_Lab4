7c995bf9b46d9557efc4162f06817270426db1c8
David.O <46855679+DDRBernal@users.noreply.github.com>
2020-02-06 10:42:32 -0500

    Add files via upload

---
+++ LigthwayContainer_DependencyInjection_Hangman/src/test/GameScoreTest.java
@@ -0,0 +1,19 @@
+package main;
+
+import org.junit.Test;
+
+import org.junit.Assert;
+
+public class GamesScoreTest {
+
+    public GamesScoreTest(){
+
+    }
+
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        Assert.assertTrue( 2==2 );
+    }
+
+}
